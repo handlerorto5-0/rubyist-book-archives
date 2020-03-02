0fc701c46888b7492f2d98bcb71bfa10b6df71e5
KVexcavator <str_token@mail.ru>
2020-03-02 19:34:41 +0300

    loops

---
+++ 012_loops/while.rb
@@ -0,0 +1,19 @@
+n = 1
+while n<11
+  puts n
+  n = n+1
+end
+puts "Done!"
+puts "------------------"
+n=1
+begin
+  puts n
+  n = n+1
+end while n<11
+puts "Done!"
+puts "------------------"
+n=1
+n=n+1 until n==10
+putst "We've reached 10!"
+puts "------------------"
+
